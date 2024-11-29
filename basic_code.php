67a6f1a6f755d9e8c329d5909c53b7439d7aab7e
mimjamam <monmoy324@gmail.com>
2024-11-29 23:12:48 +0600

    norhing

--- basic_code.php
+++ basic_code.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 $txt ="Hello world !!";
 $x=5;
@@ -6,4 +6,27 @@ $y=6;
 echo "I love " .$txt . "<br>";
 echo $x + $y;
 
+?> -->
+
+<!-- //classes and objects -->
+<?php
+class Fruit{
+    public $name;
+
+    function set_name($name){
+        $this->name=$name;
+    }
+    function get_name(){
+        return $this->name;
+    }
+}
+
+$apple=new Fruit();
+
+$apple ->set_name('Apple');
+
+echo $apple -> get_name();
+
 ?>
+
+
